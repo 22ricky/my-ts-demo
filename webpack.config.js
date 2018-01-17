612d5ce5c63677d569fe3c221fb2958d19f1488e
22ricky <wyprivatemail@163.com>
2018-01-17 14:32:44 +0800

    Adding css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,9 @@ module.exports = {
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      // All files with a '.css' extension will be handled by 'css-loader'.
+      { test: /\.css$/, loader: "css-loader" },
     ]
   },
 
@@ -26,8 +28,8 @@ module.exports = {
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDom"
-  },
+  // externals: {
+  //   "react": "React",
+  //   "react-dom": "ReactDom"
+  // },
 };
